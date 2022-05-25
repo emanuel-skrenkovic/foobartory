7e4fb27d2137bcc25a8f5b73f213a310401cfb9a
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-05-25 23:24:57 +0200

    fixed issue where multiple events were being started for already busy robots

--- main.py
+++ main.py
@@ -19,18 +19,26 @@ FOOBAR_SUCCESS_RATE = 0.6
 
 
 class Event:
-    def __init__(self, duration, action, on_after=None):
-        self.duration = duration * TIME_FACTOR
-        self.action   = action
-        self.on_after = on_after
+    def __init__(self, duration, action, on_before, on_after=None):
+        self.duration  = duration * TIME_FACTOR
+        self.action    = action
+        self.on_before = on_before
+        self.on_after  = on_after
 
-    def run(self, dt):
-        def run():
+    def run(self):
+        # Use on before to set the Robot.busy to True
+        # on run, instead of on creating the event.
+        # This avoids the issue where multiple events are
+        # started before busy is set to True, which is
+        # obviously incorrect.
+        self.on_before()
+
+        def run_action():
             self.action()
             if self.on_after is not None:
                 self.on_after()
 
-        timer = Timer(self.duration, run)
+        timer = Timer(self.duration, run_action)
         timer.start()
         return timer
 
@@ -39,29 +47,26 @@ class Robot:
     def __init__(self):
         self.events = []
         self.busy   = False
-        self.work   = None
-
+        self.work   = 'foo'
 
     def foo(self, state):
         def action():
             state['foo'] += 1
-            # print('foo')
 
         self._schedule_event(
             'foo',
-            Event(1, action, self._after_action),
+            self._create_event(1, action),
             state
         )
 
     def bar(self, state):
         def action():
             state['bar'] += 1
-            # print('bar')
 
         duration = round(random.uniform(0.5, 2.0), 1)
         self._schedule_event(
             'bar',
-            Event(duration, action, self._after_action),
+            self._create_event(duration, action),
             state
         )
 
@@ -78,65 +83,62 @@ class Robot:
             else:
                 state['bar'] += 1
 
-            # print('foobar')
-
         self._schedule_event(
             'foobar',
-            Event(2, action, self._after_action),
+            self._create_event(2, action),
             state
         )
-
-    def _change_jobs(self, job, state):
-        self.work = 'changing'
-
-        def action():
-            previous_work = self.work
-
-            self.work = job
-
-            # print(f'switched job from {previous_work} to {self.work}')
-
-        self.events.append(Event(10, action))
 
     def sell_foobars(self, state):
         state['foobar'] -= 5
 
-        def action():
-            state['money'] += 5
+        for _ in range(5):
+            def action():
+                state['money'] += 1
 
-        self._schedule_event(
-            'selling',
-            Event(10, action),
-            state
-        )
+            self._schedule_event(
+                'selling',
+                self._create_event(2, action),
+                state
+            )
 
     def buy_new_robot(self, state):
-        # TODO: very ugly and just not the way I want it to work.
-        if state['foo'] >= 6 and state['money'] >= 3:
-            state['money'] -= 3
-            state['foo']   -= 6
-
-            spent_money = True
+        state['money'] -= 3
+        state['foo']   -= 6
 
         def action():
-            if spent_money:
-                state['robots'].append(Robot())
+            state['robots'].append(Robot())
 
         self._schedule_event(
             'shopping',
-            Event(1, action, self._after_action),
+            self._create_event(1, action),
             state
         )
+
+    def _change_jobs(self, job):
+        self.work = 'changing'
+
+        def action():
+            self.work = job
+
+        self.events.append(
+            self._create_event(5, action)
+        )
+
+    def _create_event(self, duration, action):
+        return Event(duration, action, self._before_action, self._after_action)
+
+    def _before_action(self):
+        self.busy = True
 
     def _after_action(self):
         self.busy = False
 
     def _schedule_event(self, name, event, state):
         if self.work != name:
-            self._change_jobs(name, state)
+            self._change_jobs(name)
 
         self.events.append(event)
-        self.busy = True
 
 
 class Game:
@@ -157,20 +159,16 @@ class Game:
         # simple game loop
         while not self._win_condition():
             current_time = time.time()
-            dt = current_time - last_frame_time
+            # dt = current_time - last_frame_time
             last_frame_time = current_time
 
             sleep_time = 1./FPS - (current_time - last_frame_time)
             if sleep_time > 0:
                 time.sleep(sleep_time)
 
-            self._game(dt * TIME_FACTOR)
+            self._game()
             self.running_events = [e for e in self.running_events if e.is_alive()]
-
-            assert self.state['foo'] >= 0
-            assert self.state['foo'] >= 0
-            assert self.state['foobar'] >= 0
-            assert self.state['money'] >= 0
+            self._assert_state()
 
         for running in self.running_events:
             running.cancel()
@@ -178,7 +176,7 @@ class Game:
         self._refresh_display()
         print('Finished!')
 
-    def _game(self, dt):
+    def _game(self):
         # Start moving robots to shopping if enough resources are
         # being collected.
         if self.state['money'] >= 3 and self.state['foo'] >= 6:
@@ -197,26 +195,35 @@ class Game:
             if robot.work == 'shopping':
                 continue
 
-            # A very elaborate fizzbuzz.
+            if robot.busy:
+                continue
+
+            if robot.events:
+                continue
+
+            # Fizzbuzz!
             if self.state['foobar'] >= 10:
                 robot.sell_foobars(self.state)
-            elif self.state['foo'] > 6 and self.state['bar'] > 6:
+            elif self.state['foo'] >= 8 and self.state['bar'] >= 2:
                 robot.foobar(self.state)
             elif self.state['foo'] >= 10:
                 robot.bar(self.state)
             else:
                 robot.foo(self.state)
 
+        for robot in self.state['robots']:
+            if robot.busy:
+                continue
+
+            if robot.events:
+                event = robot.events.pop(0)
+                task = event.run()
+                self.running_events.append(task)
+
         self._refresh_display()
 
         if self._win_condition():
             return
-
-        for robot in self.state['robots']:
-            if robot.events:
-                event = robot.events.pop(0)
-                task = event.run(dt)
-                self.running_events.append(task)
 
     def _win_condition(self):
         return len(self.state['robots']) >= 30
@@ -236,8 +243,10 @@ class Game:
         selling      = self._worker_count('selling')
         changing     = self._worker_count('changing')
 
+        assert foo + bar + foobar + shopping + selling + changing == total_robots
+
         os.system(CLEAR_COMMAND)
-        print(f'Total robots: {total_robots}')
+        print(f'Total robots: {total_robots}\n')
         print(f'Mining foo: {foo}')
         print(f'Mining bar: {bar}')
         print(f'Mining foobar: {foobar}')
@@ -251,6 +260,12 @@ class Game:
             'foobar': self.state['foobar'],
             'money':  self.state['money']
         })
+
+    def _assert_state(self):
+        assert self.state['foo'] >= 0
+        assert self.state['foo'] >= 0
+        assert self.state['foobar'] >= 0
+        assert self.state['money'] >= 0
 
 
 INITIAL_GAME_STATE = [Robot(), Robot()]
